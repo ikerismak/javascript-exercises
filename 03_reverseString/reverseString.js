6924bd0304163234a238a1ca46a71100ae63f4e4
ikerismak <ikerismak@gmail.com>
2023-06-06 01:21:19 -0600

    03,04, completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringToReverse = string.split("");
 
+  let reversedArray = stringToReverse.reverse();
+
+  let reversedString = reversedArray.join("");
+
+  return reversedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let elementsToDelete = [];
 
+  for (var i = 1; i < arguments.length; i++) {
+    elementsToDelete.push(arguments[i]);
+  }
+
+  let mainArray = arguments[0];
+
+  console.log(mainArray);
+  console.log(elementsToDelete);
+
+  let arrayWithOutElements = mainArray.filter(
+    (element) => !elementsToDelete.includes(element)
+  );
+
+  return arrayWithOutElements;
 };
 
 // Do not edit below this line
