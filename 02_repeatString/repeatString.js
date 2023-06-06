816e5ee9200d0985d7f3fa2b2f16ed8968e95b52
ikerismak <ikerismak@gmail.com>
2023-06-06 00:42:13 -0600

    01, 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, times) {
+  try {
+    if (times < 0) {
+      return "ERROR";
+    }
 
+    let stringRepeated = string.repeat(times);
+    return stringRepeated;
+  } catch (error) {
+    return error;
+  }
 };
 
 // Do not edit below this line
